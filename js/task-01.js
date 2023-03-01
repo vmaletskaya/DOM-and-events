5309e58613eb2da40a18e6d5d59bb687f3970475
Vika Maletskaya <v.maletskayaah@gmail.com>
2023-03-01 17:42:16 +0300

    fixed 1 tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,11 @@
 
+  
 const categoriesRefs = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesRefs.length}`);
 
 categoriesRefs.forEach((category) => {
-    const categoryTitle = category.querySelector('h2').textContent;
-    const categoryElements = category.querySelectorAll('li').length;
-    console.log(`Category: ${categoryTitle}`);
-    console.log(`Elements: ${categoryElements}`);
-  });
+const categoryTitle = category.firstElementChild.textContent;
+const categoryElements = category.lastElementChild.children.length;
+console.log(`Category: ${categoryTitle}`);
+console.log(`Elements: ${categoryElements}`);
+});
